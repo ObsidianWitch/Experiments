bfd406a377077381dafeb4e8e9267aea292c5530
Obsidienne <obsidienne@protonmail.com>
2020-10-22 00:28:59 +0200

    IkenfellExtractor:extract.cs: img2pngs

---
+++ IkenfellExtractor/extract.cs
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S csharp -pkg:dotnet -lib:Ikenfell -r:GameEngine.dll,LittleWitch.dll -s
+
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using GE = GameEngine;
+using LW = LittleWitch;
+
+static class Extractor {
+    // Note: Couldn't use GE.Bitmap.Save(), it throws the following exception:
+    // "System.DllNotFoundException: utilities.dll [...]". And indeed, the DLL
+    // is missing from the game directory.
+    public static void img2png(GE.Bitmap img, String fileout) {
+        var data = img.Data.SelectMany(item =>
+            new [] { item.B, item.G, item.R, item.A }).ToArray();
+
+        unsafe { fixed(byte* ptr = data) {
+            var bitmap = new Bitmap(
+                width:  img.Width,
+                height: img.Height,
+                stride: 4 * img.Width,
+                format: PixelFormat.Format32bppArgb,
+                scan0:  (IntPtr) ptr);
+            bitmap.Save(fileout);
+        }}
+    }
+
+    public static void imgs2pngs(String dirin, String dirout) {
+        var imagePaths = Directory.GetFiles($"{dirin}/Atlas/", "*.img");
+        foreach (var path in imagePaths) {
+            var img = new GE.Bitmap(path);
+            var stem = Path.GetFileNameWithoutExtension(path);
+            img2png(img, $"{dirout}/i_{stem}.png");
+        }
+    }
+}
+
+// init
+GE.Platform.Init();
+GE.Platform.ContentRoot = "Ikenfell/"
+GE.Platform.SaveRoot = "Out/";
+var game = new LW.Game();
+
+// extract
+Extractor.imgs2pngs(GE.Platform.ContentRoot, GE.Platform.SaveRoot);
