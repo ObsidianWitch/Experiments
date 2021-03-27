f2964f02ad4435c48f4387b9e18101052e27a91e
Obsidienne <obsidienne@protonmail.com>
2021-03-28 00:10:07 +0100

    SetGame: Game loop

--- Snippets/SetGame.py
+++ Snippets/SetGame.py
@@ -11,24 +11,41 @@
 # * board: 12 cards
 # * deck: 81 cards, covers all the possible combination of features once
 
-import curses, itertools, random
+import itertools, random
 
 class Game:
-    OPTIONS = {
+    TXT_RED   = '\033[91m'
+    TXT_GREEN = '\033[92m'
+    TXT_BLUE  = '\033[94m'
+    TXT_NORMAL    = ''
+    TXT_BOLD      = '\033[1m'
+    TXT_UNDERLINE = '\033[4m'
+    TXT_RESET = '\033[0m'
+
+    CARD_OPTIONS = {
         'letter':'ABC',
         'number': (1, 2, 3),
-        'color': (1, 2, 3),
-        'emphasis': (curses.A_NORMAL, curses.A_BOLD, curses.A_UNDERLINE),
+        'color': (TXT_RED, TXT_GREEN, TXT_BLUE),
+        'emphasis': (TXT_NORMAL, TXT_BOLD, TXT_UNDERLINE),
     }
 
     def __init__(self):
         self.deck = list(
-            dict(zip(self.OPTIONS.keys(), values))
-            for values in itertools.product(*self.OPTIONS.values())
+            dict(zip(self.CARD_OPTIONS.keys(), values))
+            for values in itertools.product(*self.CARD_OPTIONS.values())
         )
         random.shuffle(self.deck)
 
         self.deck, self.board = self.deck[:-12], self.deck[-12:]
 
-game = Game()
-print(len(game.deck), len(game.board)) # DEBUG
+    def print_board(self):
+        for i, card in enumerate(self.board):
+            print(f'{i}) {card["color"]}{card["emphasis"]}'
+                + f'{card["letter"] * card["number"]}{self.TXT_RESET}')
+
+    def loop(self):
+        self.print_board()
+        while (answer := input('>>> ')) != 'q':
+            self.print_board()
+
+Game().loop()
