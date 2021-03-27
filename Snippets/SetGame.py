315f21dbf8b06e84cd8e7977b5cc97eba24b9018
Obsidienne <obsidienne@protonmail.com>
2021-03-27 21:33:49 +0100

    SetGame: init + rules

---
+++ Snippets/SetGame.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# https://en.wikipedia.org/wiki/Set_(card_game)
+# * cards: vary in 4 features of 3 possibilities each
+#   * letter: A, B, C
+#   * color: red, green, blue
+#   * number: 1, 2, 3
+#   * emphasis: normal, bold, underline
+# * set: contains 3 cards which for each feature, the feature needs to be the
+#   same or different
+# * board: 12 cards
+# * deck: 81 cards, covers all the possible combination of features once
